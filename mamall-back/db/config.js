5dd227f84f9b03b3aadfc9f716525c864363c251
Mopahta <novoseletskiy03@mail.ru>
2023-03-20 17:41:37 +0300

    db config

---
+++ mamall-back/db/config.js
@@ -0,0 +1,11 @@
+// postgres database config
+
+module.exports = {
+
+    pghost: process.env.PGHOST,
+    pguser: process.env.PGUSER,
+    pgdatabase: process.env.PGDATABASE,
+    pgpassword: process.env.PGPASSWORD,
+    pgport: process.env.PGPORT
+    
+}
