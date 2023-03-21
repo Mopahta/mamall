7cde20ebe6d255edda0d5165b7fef13860e38221
Mopahta <novoseletskiy03@mail.ru>
2023-03-21 23:02:58 +0300

    db service connect and disconnect

---
+++ mamall-back/db/db.js
@@ -0,0 +1,64 @@
+const pg = require('pg');
+const config = require('./config/config');
+const users = require('./user/db-users');
+
+(function() {
+    let db;
+    let pool;
+
+    module.exports.connect = function() {
+        pool = new pg.Pool({
+
+            host: config.pghost,
+            user: config.pguser,
+            database: config.pgdatabase,
+            password: config.pgpassword,
+            port: config.pgport,
+            max: 20,
+            idleTimeoutMillis: 35000
+
+        });
+
+        users.setPool(pool)
+
+        pool.on('connect', (client) => {
+            console.log("New client connected to database.");
+        });
+
+        pool.on('error', (error, client) => {
+            console.error(error);
+        });
+
+        pool.on('remove', (client) => {
+            console.log("Client was removed from pool.");
+        });
+
+        console.log('Successfully created database Pool.')
+        
+        return true;
+    }
+
+    module.exports.disconnect = function() {
+        (async () => {
+            await pool.end()
+                .then(() => { 
+                    console.log('The database connection is closed.');
+                    return true;
+                })
+                .catch((err) => { 
+                    console.error(err);
+                    return false;
+                })  
+        })();
+        
+        return true;
+    }
+
+    module.exports.getAllUsers = users.getAllUsers 
+
+    module.exports.getUserInfoById = users.getUserInfoById 
+
+    module.exports.getUserOnlineStatus = users.getUserOnlineStatus
+
+    module.exports.getUserContacts = users.getUserContacts
+}());
