5949e6ef53f3a0dd7bac485d0a3c1ee94cc20aa7
Mopahta <novoseletskiy03@mail.ru>
2023-03-24 20:14:15 +0300

    react use websocket base

--- mamall-front/src/App.js
+++ mamall-front/src/App.js
@@ -1,11 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './common/Header';
+import { useState, useCallback, useEffect } from 'react';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 
-function App() {
-    return (
-        <Header/>
+function App () {
+    //Public API that will echo messages sent to it back to the client
+    const [socketUrl, setSocketUrl] = useState('ws://localhost:8000/');
+    const [messageHistory, setMessageHistory] = useState([]);
+
+    const { sendMessage, lastMessage, readyState } = useWebSocket(
+        socketUrl, {
+            onOpen: () => console.log('opened'),
+            protocols: 'mamall-signal-protocol'
+    });
+
+    useEffect(() => {
+        if (lastMessage !== null) {
+        setMessageHistory((prev) => prev.concat(lastMessage));
+        }
+    }, [lastMessage, setMessageHistory]);
+
+    const handleClickChangeSocketUrl = useCallback(
+        () => setSocketUrl('wss://demos.kaazing.com/echo'),
+        []
     );
-}
+
+    const handleClickSendMessage = useCallback(() => { 
+        sendMessage('Hello');
+        setTimeout(handleClickSendMessage, 1000);
+    }, []);
+
+    const connectionStatus = {
+        [ReadyState.CONNECTING]: 'Connecting',
+        [ReadyState.OPEN]: 'Open',
+        [ReadyState.CLOSING]: 'Closing',
+        [ReadyState.CLOSED]: 'Closed',
+        [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+    }[readyState];
+
+    return (
+        <>
+            <Header/>
+        {/* <button onClick={handleClickChangeSocketUrl}>
+            Click Me to change Socket Url
+        </button> */}
+        <button
+            onClick={handleClickSendMessage}
+            disabled={readyState !== ReadyState.OPEN}
+        >
+            Click Me to send 'Hello'
+        </button>
+        <span>The WebSocket is currently {connectionStatus}</span>
+        {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
+        <ul>
+            {messageHistory.map((message, idx) => (
+            <span key={idx}>{message ? message.data : null}</span>
+            ))}
+        </ul>
+        </>
+    );
+};
 
 export default App;
