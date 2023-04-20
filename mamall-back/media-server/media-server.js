2ea1fe574b802a48662e301acc76e0674ccbdfd8
Mopahta <novoseletskiy03@mail.ru>
2023-04-20 23:40:42 +0300

    room routers and transports creation

--- mamall-back/media-server/media-server.js
+++ mamall-back/media-server/media-server.js
@@ -1,9 +1,11 @@
 process.env.DEBUG = "mediasoup*";
 
 const mediasoup = require("mediasoup");
+const { v4: uuidv4 } = require('uuid');
 
 let worker;
 let webRtcServer;
+let routers = [];
 
 
 async function init() {
@@ -28,6 +30,7 @@ async function init() {
             }
         ]
     })
+
 }
 
 async function stop() {
@@ -36,16 +39,92 @@ async function stop() {
     }
 }
 
-async function createRoom() {
+async function createRoom(room_id) {
     if (worker == null) {
         console.error("No worker created.");
         return;
     }
 
-    worker.create
+    let router = await worker.createRouter({
+        mediaCodecs:
+        [
+            {
+                kind        : "audio",
+                mimeType    : "audio/opus",
+                clockRate   : 48000,
+                channels    : 2
+            },
+            {
+			    kind       : 'audio',
+			    mimeType   : 'audio/multiopus',
+			    clockRate  : 48000,
+			    channels   : 4,
+			    // Quad channel.
+			    parameters :
+			    {
+				    'channel_mapping' : '0,1,2,3',
+				    'num_streams'     : 2,
+				    'coupled_streams' : 2
+			    }
+		    },
+            {
+			    kind                 : 'audio',
+			    mimeType             : 'audio/PCMU',
+			    clockRate            : 8000,
+            },
+            {
+			    kind         : 'audio',
+			    mimeType     : 'audio/ISAC',
+			    clockRate    : 16000,
+		    },
+        ],
+    })
+
+    routers.push({
+        room_id: room_id,
+        router: router,
+        transports: []
+    });
+
+    return router;
+}
+
+async function getRoomRouterCapabilities(room_id) {
+    let roomRouter = routers.find(x => x.room_id == room_id);
+    return roomRouter.router.rtpCapabilities;
+}
+
+async function joinRoom(room_id) {
+    let roomRouter = routers.find(x => x.room_id == room_id);
+
+    if (roomRouter == null) {
+        await createRoom(room_id);
+    }
+
+    let webRtcTransport = await roomRouter.createWebRtcTransport({
+        webRtcServer: webRtcServer,
+        enableUdp    : true,
+        enableTcp    : true,
+        preferUdp    : true
+    })
+
+
+    let routerIndex = routers.findIndex(x => x.room_id == room_id);
+    routers[routerIndex].transports.push(webRtcTransport);
+
+    return {
+        id: uuidv4(),
+        iceParameters: webRtcTransport.iceParameters,
+        iceCandidates: webRtcTransport.iceCandidates,
+        dtlsParameters: webRtcTransport.dtlsParameters,
+        sctpParameters: webRtcTransport.sctpParameters
+    }
 }
 
 module.exports = {
     init: init,
-    stop: stop
+    stop: stop,
+    createRoom: createRoom,
+    joinRoom: joinRoom,
+    getRtpCapabilities: getRoomRouterCapabilities
 }
