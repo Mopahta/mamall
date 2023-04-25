c413e57d93e63e6bfe87110501f547149c9ab16b
Mopahta <novoseletskiy03@mail.ru>
2023-04-25 17:29:11 +0300

    media server transport connection and produce options abandoned routers and transports collection

--- mamall-back/media-server/media-server.js
+++ mamall-back/media-server/media-server.js
@@ -18,19 +18,20 @@ async function init() {
         [
             {
                 protocol : 'udp',
-                ip       : '0.0.0.0',
-                announcedIp: '127.0.1.1',
+                ip       : process.env.MEDIASOUP_LISTEN_IP || '192.168.1.104',
+                announcedIp: process.env.MEDIASOUP_ANNOUNCED_IP,
                 port     : 11111
             },
             {
                 protocol : 'tcp',
-                ip       : '0.0.0.0',
-                announcedIp: '127.0.1.1',
+                ip       : process.env.MEDIASOUP_LISTEN_IP || '192.168.1.104',
+                announcedIp: process.env.MEDIASOUP_ANNOUNCED_IP,
                 port     : 11111
             }
         ]
     })
 
+    checkRouterTransportAvailability();
 }
 
 async function stop() {
@@ -102,7 +103,7 @@ async function getRoomRouterCapabilities(room_id) {
     return roomRouter.router.rtpCapabilities;
 }
 
-async function joinRoom(room_id) {
+async function createMediaTransport(room_id) {
     let roomRouter = routers.find(x => x.room_id == room_id);
 
     if (roomRouter == null) {
@@ -113,15 +114,20 @@ async function joinRoom(room_id) {
         webRtcServer: webRtcServer,
         enableUdp    : true,
         enableTcp    : true,
-        preferUdp    : true
+        preferUdp    : true,
+        appData      : { transportId: uuidv4() }
     })
 
 
     let routerIndex = routers.findIndex(x => x.room_id == room_id);
-    routers[routerIndex].transports.push(webRtcTransport);
+    routers[routerIndex].transports.push(
+        { 
+            transportId: webRtcTransport.appData.transportId,
+            webRtcTransport: webRtcTransport
+        });
 
     return {
-        id: uuidv4(),
+        id: webRtcTransport.appData.transportId,
         iceParameters: webRtcTransport.iceParameters,
         iceCandidates: webRtcTransport.iceCandidates,
         dtlsParameters: webRtcTransport.dtlsParameters,
@@ -129,11 +135,44 @@ async function joinRoom(room_id) {
     }
 }
 
+async function connectTransport(room_id, transportId, dtlsParameters) {
+    let roomRouter = routers.find(x => x.room_id == room_id);
+    if (roomRouter != null) {
+        let routerTransport = roomRouter.transports.find(x => x.transportId == transportId);
+        console.log(routerTransport);
+
+        await routerTransport.webRtcTransport.connect({ dtlsParameters });
+    }
+}
+
+async function produceTransport(room_id, transportId, kind, rtpParameters) {
+    let roomRouter = routers.find(x => x.room_id == room_id);
+
+    if (roomRouter != null) {
+        let routerTransport = roomRouter.transports.find(x => x.transportId == transportId);
+
+        const producer = await routerTransport.webRtcTransport.produce({ kind, rtpParameters });
+    }
+
+}
+
+function checkRouterTransportAvailability() {
+    routers = routers.filter(router => !router.router.closed);
+    routers.forEach(router => {
+        router.transports = router.transports.filter(transport => !transport.webRtcTransport.closed)
+    })
+    setTimeout(checkRouterTransportAvailability, 600 * 1000);
+
+    console.log(`${new Date()} Routers amount: ${routers.length}`);
+}
+
 module.exports = {
     init: init,
     stop: stop,
     routers: routers,
+    getRtpCapabilities: getRoomRouterCapabilities,
     createRoom: createRoom,
-    joinRoom: joinRoom,
-    getRtpCapabilities: getRoomRouterCapabilities
+    createMediaTransport: createMediaTransport,
+    connectTransport: connectTransport,
+    produceTransport: produceTransport
 }
