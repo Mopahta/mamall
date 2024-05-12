79222520ca89420d5c550a5c3d556b05b3f4e5a3
Mopahta <novoseletskiy03@mail.ru>
2024-05-12 16:41:22 +0300

    feat: jwt auth filter

---
+++ mamall-back/rest-api/v2/rest-api-v2/src/main/java/tech/mamall/config/JwtAuthenticationFilter.java
@@ -0,0 +1,64 @@
+package tech.mamall.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import tech.mamall.model.UserEntity;
+import tech.mamall.service.JwtService;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+	private final JwtService jwtService;
+
+	// TODO: jwt filter
+	// TODO: base rest api functionality
+	@Override
+	@SneakyThrows
+	protected void doFilterInternal(HttpServletRequest request,
+						  @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) {
+		String token = null;
+
+		if (request.getCookies() != null) {
+			for (Cookie cookie : request.getCookies()) {
+				if (cookie.getName().equals("token")) {
+					token = cookie.getValue();
+				}
+			}
+		}
+
+		if (token == null) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+
+		UserEntity user = jwtService.extractUserInfoFromToken(token);
+
+		if(user.getUsername() != null) {
+			SecurityContext context = SecurityContextHolder.createEmptyContext();
+
+			UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+				  user,
+				  null,
+				  user.getAuthorities()
+			);
+
+			authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+			context.setAuthentication(authToken);
+			SecurityContextHolder.setContext(context);
+		}
+
+		filterChain.doFilter(request, response);
+	}
+}
